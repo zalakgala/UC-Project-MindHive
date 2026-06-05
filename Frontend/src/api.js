bdb8766c40443c1e7ef31c44d81e4f7f56903c9c
Zalak Gala <zalakgala06@gmail.com>
2026-06-05 11:27:45 +0530

    Backend Deployed link

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://uc-project-mindhive.onrender.com',
   // withCredentials: true // Uncomment if you want to also send session cookies
 });
 
