fff09896fd0a3e4f2825950215cb30af39d87755
Zalak Gala <zalakgala06@gmail.com>
2026-06-05 12:27:17 +0530

    Connected To PostgreSQL

--- Backend/UC-project-main/notes/settings.py
+++ Backend/UC-project-main/notes/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
